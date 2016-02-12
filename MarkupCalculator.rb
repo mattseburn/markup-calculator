380c28eaf4987cf43e3cda7b8b89145c4159ce78
Matt Seburn <matt@seburn.net>
2016-02-12 02:34:07 -0500

    move validation logic to separate function

--- MarkupCalculator.rb
+++ MarkupCalculator.rb
@@ -8,5 +8,13 @@ class MarkupCalculator
     }
 
     def calculate(base_price, num_people, goods)
+        raise RuntimeError, "Invalid input" unless is_valid(base_price, num_people, goods)
+    end
+
+    private def is_valid(base_price, num_people, goods)
+        num_people > 0 and
+            GOODS.value?(goods) and
+            (base_price.is_a? Integer or base_price.is_a? Float) and
+            base_price >= 0
     end
 end
